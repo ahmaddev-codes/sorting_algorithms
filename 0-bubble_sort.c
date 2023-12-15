71e0346bf1d6a95ebffd3cba579b854139d90248
iahm-codes <omegabliss23@.com>
2023-12-15 16:27:50 +0100

    Code: corrcted code in bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,20 +12,20 @@
   */
 void bubble_sort(int *array, size_t size)
 {
-	int i, aux = 0, max = 0, swapped = 1;
+	int i = 0, temp = 0, max = 0, swapped;
 
 	if (!array || size < 2)
 		return;
 
 	max = size - 1;
 
-	for (i = 0; i < max; ++i)
+	for (; i < max; ++i)
 	{
 		if (array[i] > array[i + 1])
 		{
-			aux = array[i];
+			temp = array[i];
 			array[i] = array[i + 1];
-			array[i + 1] = aux;
+			array[i + 1] = temp;
 			swapped = 1;
 			print_array(array, size);
 		}
