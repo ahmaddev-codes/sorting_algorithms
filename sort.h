2e99c714c0011f840c76d870847c1c4819b88b12
iahm-codes <omegabliss23@.com>
2023-12-15 16:00:31 +0100

    Code: updated header file

--- sort.h
+++ sort.h
@@ -1,5 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,4 +23,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+
 #endif /* SORT_H */
